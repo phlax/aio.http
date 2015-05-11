c76d97d7a759992f11966ecd7185ee2d1c096283
Ryan Northey <ryan@space23.net>
2015-05-11 09:13:34 +0000

    converting test to use string config

--- aio/http/tests/test_server.py
+++ aio/http/tests/test_server.py
@@ -9,19 +9,6 @@ from aio.signals import Signals
 import aio.app
 from aio.app.runner import runner
 
-TEST_DIR = os.path.dirname(__file__)
-
-
-@asyncio.coroutine
-def handle_hello_world(app):
-    return aiohttp.web.Response(body=b"Hello, world")
-
-
-@asyncio.coroutine
-def test_root(app):
-    app.router.add_route("GET", "/", handle_hello_world)
-
-
 HTTP_CONFIG = """
 [aio:commands]
 run: aio.app.cmd.cmd_run
@@ -32,6 +19,16 @@ root: aio.http.tests.test_server.test_root
 address: 0.0.0.0
 port: 7070
 """
+
+@asyncio.coroutine
+def test_root(webapp):
+
+    @asyncio.coroutine
+    def handle_hello_world(webapp):
+        return aiohttp.web.Response(body=b"Hello, world")
+
+    webapp.router.add_route("GET", "/", handle_hello_world)
+
 
 class HttpServerTestCase(AioAppTestCase):
 
